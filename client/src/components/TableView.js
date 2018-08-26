7667b016ba9eaca1797af6281bcf08eea734c48a
mark hopcraft <mark82hopcraft@gmail.com>
2018-08-26 21:04:09 +0100

    tableview

--- client/src/components/TableView.js
+++ client/src/components/TableView.js
@@ -5,6 +5,8 @@ class TableView extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      selectVal: '',
+      tableName: '',
       rows: []
     }
     this.BASEURL = `http://localhost:3002/`
@@ -29,33 +31,56 @@ class TableView extends Component {
         'Content-Type': 'application/json; charset=utf-8'
       }
     }
+
     fetch(`${this.BASEURL}api/table/categories/`, OPTIONS)
       .then(res => {
         return res.json()
       })
-      .then(json => {//setState tableName as well
-        return json.rows
-      })
-      .then(rows=> {
-        this.setState({rows: rows})
+      .then(json => {
+        this.setState({
+          tableName: json.tableName,
+          rows: json.rows
+        })
       })
       .catch(err => {
-        //console.log(err)
+        console.log(err)
       })
+  }
+
+  tableSelect(event) {
+    this.setState({selectVal: })
   }
 
   render() {
     return(
-      <table>
-        <tbody>
-          {this.state.rows.map(row =>
-            <tr key={row.category_id}>
-              <td>{row.category_id}</td>
-              <td>{row.category}</td>
+      <div>
+        <select onChange={this.tableSelect} value={this.state.selectVal}>
+          <option value=''>none</option>
+          <option value='todo'>todo</option>
+          <option value='progresses'>progresses</option>
+          <option value='categories'>categories</option>
+        </select>
+
+        <table>
+          <thead>
+            <tr>
+              <th colSpan='2'>{this.state.tableName}</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+            <tr>
+              <th>category_id</th>
+              <th>category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.rows.map(row =>
+              <tr key={row.category_id}>
+                <td>{row.category_id}</td>
+                <td>{row.category}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
